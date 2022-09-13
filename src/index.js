cc1acb01744034d7c3118b526c2b3670e5ad488a
Heldenglanz <heldenglanz@gmail.com>
2022-09-13 18:06:20 +0300

    touch start

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+
+console.log('start????')
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
